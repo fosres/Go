bb825448ea303eb189a5bc1864d82de2b2df225b
Tanveer Salim <Tanveer.Salim@ttu.edu>
2019-11-15 10:08:23 -0600

    Got rid of unneccesary code in fetch.go

--- Go_Donovan/fetch.go
+++ Go_Donovan/fetch.go
@@ -41,39 +41,12 @@ func getPages(a []string) [1024][]byte {
 	
 	return ans
 }
+
 func main() {
 	
 
 	i := 0 
 
-/*
-	var ans [1024][]byte
-
-	for _, url := range os.Args[1:] {
-		resp, err := http.Get(url)
-
-		if err != nil {
-			fmt.Fprintf(os.Stderr,"fetch: %v\n",err)
-			os.Exit(1)
-		}
-
-		b, err := ioutil.ReadAll(resp.Body)
-
-		resp.Body.Close()
-
-		if err != nil {
-			fmt.Fprintf(os.Stderr,"fetch: reading %s: %v\n",url,err)
-			os.Exit(1)
-		}
-
-		ans[i] = b
-
-		i++
-		
-	}
-	
-	i = 0
-*/
 	c := getPages(os.Args)
 
 	for c[i] != nil {
